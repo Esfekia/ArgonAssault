c0a2a6548f59ec674e62ee6bca6d7f2137af9d1c
Sertac <sertac@gmail.com>
2023-02-19 13:52:08 -0600

    Pitch, Yaw and Roll effects are added & optimized.

--- Assets/_core/Scripts/PlayerController.cs
+++ Assets/_core/Scripts/PlayerController.cs
@@ -6,14 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] InputAction movement;
-    [SerializeField] float controlSpeed = 0.5f;
-    [SerializeField] float xRange = 6f;
-    [SerializeField] float yRange = 4f;
-    
+    [SerializeField] float controlSpeed = 0.25f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
+
+    [SerializeField] float positionPitchFactor = 2.5f;
+    [SerializeField] float positionYawFactor = -2.5f;
+
+    [SerializeField] float controlRollFactor = -7.5f;
+    [SerializeField] float controlPitchFactor = 7.5f;
+
+    float horizontalThrow;
+    float verticalThrow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void OnEnable()
@@ -29,27 +38,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    void ProcessTranslation()
+    {
+        horizontalThrow = movement.ReadValue<Vector2>().x;
+        verticalThrow = movement.ReadValue<Vector2>().y;
 
         float rawXPos = transform.localPosition.x + (horizontalThrow * controlSpeed);
         float rawYPos = transform.localPosition.y + (verticalThrow * controlSpeed);
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-        float clampedYPos = Mathf.Clamp(rawYPos, -yRange,yRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3
             (clampedXPos,
-            clampedYPos, 
+            clampedYPos,
             transform.localPosition.z);
 
         //Old Input System
         //float horizontalThrow = Input.GetAxis("Horizontal");
         //float verticalThrow = Input.GetAxis("Vertical");
         //Debug.Log("Horizontal: " + horizontalThrow + "Vertical: " + verticalThrow);
+    }
 
+    void ProcessRotation()
+    {
+        // pitch has both position and control variable
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = verticalThrow * controlPitchFactor;
 
+        // yaw has only position variable
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
 
+        // roll has only control variable
+        float rollDueToControl = horizontalThrow * controlRollFactor;
 
-
+        float pitch =  pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToControl;
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
