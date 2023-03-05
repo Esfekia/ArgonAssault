820e0c33d96a35566d303de6a21ce1c564513fca
Esfekia <sertac@gmail.com>
2023-03-05 09:21:18 -0500

    When the player ship collides with another object, it disables controls and restarts the level after 1 second.

--- Assets/_core/Scripts/CollisionHandler.cs
+++ Assets/_core/Scripts/CollisionHandler.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    public bool isTransitioning = false;
     // Class to print out what the player object has collided with.
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(name + " has triggered " + other.gameObject.name);
+        StartCrashSequence();
     }
 
-    void OnCollisionEnter(Collision collision)
+    void StartCrashSequence()
     {
-        Debug.Log(name + " has collided with " + collision.gameObject.name);
+        isTransitioning = true;
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        // Reload the current level.
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 
 
